72ba4d4935ce8c55847c99eeeea091b056cc8f63
amaal123-123 <elbaz055299@gmail.com>
2026-08-25 04:18:15 +0300

    Add files via upload

---
+++ task2.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+ print("enter your frist name :");
+ String fn=stdin.readLineSync()!;
+ print("enter your last name:");
+ String ln=stdin.readLineSync()!;
+ print("\n full name :$fn$ln");
+
+
+}

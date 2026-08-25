4e3689e7a9975b0cf1552dd44cc16e5adeb12f63
amaal123-123 <elbaz055299@gmail.com>
2026-08-25 04:11:19 +0300

    Add files via upload

--- task1.dart
+++ task1.dart
@@ -4,13 +4,15 @@ void main() {
  print ("enter your name :");
  String name = stdin.readLineSync()!;
 
+ print("enter your age:");
+
  int age =int.parse(stdin.readLineSync()!);
 
  print("enter your grade:");
  int grade =int.parse(stdin.readLineSync()!);
- print ("\nname:$name");
+ print ("name:$name");
  print("age:$age");
  print("grade:$grade");
- 
+
 
 }
